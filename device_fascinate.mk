3dab5476fab01a09c8097aad0e0c0a6f5a52b048
jt1134 <jt1134@gmail.com>
2010-09-23 19:07:11 -0500

    s/galaxys/fascinate/

--- device_fascinate.mk
+++ device_fascinate.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-DEVICE_PACKAGE_OVERLAYS := vendor/samsung/galaxys/overlay
+DEVICE_PACKAGE_OVERLAYS := vendor/samsung/fascinate/overlay
 
 # This is the list of apps included in the generic AOSP build
 PRODUCT_PROPERTY_OVERRIDES += \
